207bbaa4cdd9e3fe8d410a58aa09eeda546b589f
sandeep.dirishala <sdirishala@uhcsr.com>
2019-11-14 14:09:44 -0600

    initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule} from "@angular/material";
+import {FormsModule} from "@angular/forms";
+
+const modules = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...modules
+  ],
+  exports: [
+    ...modules
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {Person} from "../person";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  // @ts-ignore
+  public person: Person = {lots: 1};
+  public bags: any[];
+  public lotsOfBags: any[] = [];
+  public bagWieghts: any[] = [];
+  public bagsList: any[] = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  generateLots() {
+    this.bags = Array(this.person.lots).fill(0).map((x, i) => i);
+  }
+
+  createBagsArr() {
+    console.log(this.lotsOfBags, this.person.bagsList);
+
+    this.bagsList.forEach((k, j) => {
+      console.log(this.bagsList);
+      this.lotsOfBags[j] = Array(k).fill(0).map((x, i) => i);
+      this.bagWieghts[j] = Array(k).fill(0).map((x, i) => []);
+      console.log(k, j, this.bagWieghts);
+
+    });
+
+  }
+
+  cal() {
+    console.log(this.bagWieghts.reduce((a, b) => a + b));
+  }
+
+}
